e8cf70f039a6d678d02eac2890ff050c3a36163e
Brian Neal <brianbneal@gmail.com>
2024-02-15 09:48:05 -0600

    Add project files.

---
+++ YieldDemo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace YieldDemo;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void ReadFile_Click(object sender, RoutedEventArgs e)
+    {
+        string path = Path.Combine("C:\\Users\\brian\\source\\repos\\YieldDemo\\YieldDemo\\Data.txt");
+
+        var lines = GetLinesAsync(path);
+        await foreach (string line in lines)
+        {
+            _listBox.Items.Add(line);
+        }
+    }
+
+    private async IAsyncEnumerable<string> GetLinesAsync(string path)
+    {
+        using StreamReader file = new(path);
+        
+        while (await file.ReadLineAsync() is { } line)
+        {
+            await Task.Delay(300);
+            yield return line;
+        }
+    }
+}
